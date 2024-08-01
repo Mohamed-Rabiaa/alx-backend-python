2d55f5f8e98f11bb2c7519cd30de7269295d8b93
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-01 23:34:25 +0300

    Adding 1-concat.py file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+This module contains the concat function
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    This function takes two strings and returns the
+    concatination of them
+
+    Args:
+      st1: The first string
+      str2: The second string
+
+    Returns:
+      the concatination of the two string
+    '''
+    return str1 + str2
