a4256d8c91013cec80f50b61ecbe3b04e1b6f48e
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-01 23:53:56 +0300

    Adding 3-to_str.py file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+This module contains the to_str function
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    This function takes a float and return its string representation
+
+    Args:
+      n: the float number
+
+    Returns:
+      the string representation of the float
+    '''
+    return str(n)
