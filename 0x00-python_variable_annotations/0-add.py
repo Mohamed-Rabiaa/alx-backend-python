569cd4a327025680b7a7988aa5e7668f40fb4012
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-01 22:44:49 +0300

    Adding 0x00-python_variable_annotations dir & 0-add.py file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+This module contains the add function
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    This function takes two floats and returns their sum
+
+    Args:
+    a: float
+    b: float
+
+    Return: the sum of the two numbers as a float
+    '''
+    return a + b
