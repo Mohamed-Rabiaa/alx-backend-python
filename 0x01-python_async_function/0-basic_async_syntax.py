8c142c3f770e4eb812c4d0e04e4d7a7530b4be82
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-05 08:26:16 +0300

    Adding 0-basic_async_syntax.py file

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''
+This module contains the wait_random function
+'''
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    '''
+    This function takes in an integer argument (max_delay,
+    with a default value of 10) and waits for a random delay
+    between 0 and max_delay (included and float value)
+    seconds and eventually returns it
+
+    Args:
+      max_delay (int): the max delay that the function can wait before
+      finish executing
+
+    Return:
+      the random floating number between 0 and max_delay
+    '''
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
