1f2ffe645d9582de09aad132ab72da1059c8a981
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-05 11:42:53 +0300

    Adding 4-tasks.py file

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This module contains the task_wait_n function
+"""
+
+import asyncio
+import typing
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> typing.List:
+    """
+    Create and run 'n' asyncio Tasks for the 'wait_random' coroutine
+    concurrently, and return a sorted list of the results.
+
+    Args:
+        n (int): The number of tasks to create and run concurrently.
+        max_delay (int): The maximum delay in seconds for each 'wait_random'
+        coroutine.
+
+    Returns:
+        typing.List[float]: A sorted list of delays (in seconds) from all
+        completed tasks.
+    """
+    results = await asyncio.gather(*(task_wait_random(max_delay)
+                                     for _ in range(n)))
+    return sorted(results)
