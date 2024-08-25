8f037274d4a2c6d2fb7962da85f1df5d49287912
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-25 21:00:17 +0300

    Add TestMemoize.test_memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ This module contains the TestAccessNestedMap class
 from parameterized import parameterized
 from unittest import TestCase
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from typing import Dict, Tuple, Union, Any
 
 
@@ -112,3 +112,30 @@ class TestGetJson(TestCase):
             mock_method.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
             mock_method.assert_called_once_with(test_url)
+
+
+class TestMemoize(TestCase):
+    """
+    Tests the memoize function
+    """
+    def test_memoize(self):
+        """
+        Tests the memoize function by mocking the a_method
+        function and verifies that it is only being called once
+        with the same input
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        new_testclass = TestClass()
+        with patch.object(new_testclass, 'a_method') as mock_method:
+            mock_method.return_value = 42
+            self.assertEqual(new_testclass.a_property, 42)
+            self.assertEqual(new_testclass.a_property, 42)
+            mock_method.assert_called_once()
